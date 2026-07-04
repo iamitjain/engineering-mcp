7652691e6c1e1ae0a520f39b9f2b10dde232fc9f
Amit Jain <jain.amit@securemeters.com>
2026-07-04 14:01:57 +0530

    Initial Spring boot project

---
+++ src/main/java/com/securemeters/engineering/EngineeringMcpApplication.java
@@ -0,0 +1,13 @@
+package com.securemeters.engineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineeringMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineeringMcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securemeters/engineering/EngineeringMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securemeters.engineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineeringMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
